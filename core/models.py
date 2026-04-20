2212f8bd73e552cd01e067b6d1462b694c3d760d
Takudzwa Mvere <mveretakudzwa@proton.me>
2026-04-20 14:08:29 +0200

    core, portal and models. Also connected to postgres and indtalled related dependencies

--- core/models.py
+++ core/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+#Regex logic for licenses and phone number
+
+alphanumeric = RegexValidator(
+    regex=r'^[A-Za-z0-9\s\-]+$',
+    message='Only letters, numbers, spaces and hyphens allowed'
+)
+
+numeric = RegexValidator(
+    regex=r'^\+?[0-9\s\-]+$',
+    message='Only numbers'
+)
+# Models
+class Driver(models.Model):
+    name = models.CharField(max_length=255)
+    license_no = models.CharField(max_length=255, unique=True, validators=[alphanumeric])
+    phone_no = models.CharField(max_length=10, validators=[numeric])
+    
+    def __str__(self):
+        return f"{self.name}: {self.license_no}"
+
+class Truck(models.Model):
+    STATUS_CHOICES = [
+        ('available', 'Available'),
+        ('in_transit', 'In Transit'),
+        ('maintenance', 'Maintenance'),
+    ]
+
+    registration_no =models.CharField(max_length=20, unique=True, validators=[alphanumeric])
+    capacity = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='available')
+
+    def __str__(self):
+        return f"{self.registration_no} has a capacity of {self.capacity} and is {self.status}"
+    
+class Job(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_transit', 'In Transit'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    pick_up_location = models.CharField(max_length=255)
+    delivery_location = models.CharField(max_length=255)
+    cargo = models.TextField(max_length=400)
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='pending')
+    assigned_truck = models.ForeignKey(Truck, null=True, blank=True, on_delete=models.SET_NULL, related_name='jobs')
+    assigned_driver = models.ForeignKey(Driver, null=True, blank=True, on_delete=models.SET_NULL, related_name='jobs')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Job {self.id} ==> {self.pick_up_location} to {self.delivery_location} with status {self.status}"
--- haulage/settings.py
+++ haulage/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,29 +25,31 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p^e-d-kdq*v3y&^%ur6zw_7cf_#8k(%&_g8g!33%4+7)p3w(o)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True' 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'core',
-    'ninja',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'ninja',
+    'portal',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -56,7 +63,7 @@ ROOT_URLCONF = 'haulage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,10 +82,7 @@ WSGI_APPLICATION = 'haulage.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
@@ -106,14 +110,21 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Harare'
 
 USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
