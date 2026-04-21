ceacada5ceaa235671b3270fbfe07590d2bae08f
Takudzwa Mvere <mveretakudzwa@proton.me>
2026-04-21 08:30:05 +0200

    debug

--- haulage/settings.py
+++ haulage/settings.py
@@ -20,7 +20,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+DEBUG = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
