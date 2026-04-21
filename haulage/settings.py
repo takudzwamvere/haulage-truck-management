1e28c0d1b7e02b917e0902d88514b93be8ce29a3
Takudzwa Mvere <mveretakudzwa@proton.me>
2026-04-21 07:58:16 +0200

    csrf fix for dokploy

--- haulage/settings.py
+++ haulage/settings.py
@@ -32,6 +32,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 # Application definition
 
